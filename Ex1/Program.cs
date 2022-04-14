07c469c6fa5e6dd36a8924a30f56f9b53ae4ad7a
grazimika <99834477+grazimika@users.noreply.github.com>
2022-04-14 00:32:09 -0300

    Add files via upload

---
+++ Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+      
+      
+        static void Main(string[] args)
+        {
+              solucao Ex1 = new solucao();
+
+            Console.WriteLine("Prova Diagnostica");
+            Ex1.ex1();
+            Ex1.ex2();
+            Ex1.ex3();
+            Ex1.ex4();
+            Ex1.ex5();
+         
+        }
+    }
+}
